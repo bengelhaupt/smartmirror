6f0110d613aa203b19e700e93a7a01fbd683554a
Ben-Noah Engelhaupt <code@bensoft.de>
2018-04-14 15:49:40 +0200

    Code cleanup

--- smartmirror/smartmirror.ino
+++ smartmirror/smartmirror.ino
@@ -13,6 +13,7 @@
 #include "Time.h"
 #include "Timezone.h"
 #include "dht.h"
+
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <ESP8266WebServer.h>
@@ -52,9 +53,8 @@ MDNSResponder mdns;
 
 unsigned int localPort = 2390;      // local port to listen for UDP packets
 IPAddress timeServerIP;
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-// A UDP instance to let us send and receive packets over UDP
+const int NTP_PACKET_SIZE = 48;
+byte packetBuffer[ NTP_PACKET_SIZE];
 WiFiUDP udp;
 
 // Display Buffer 128 = 64 * 16 / 8
@@ -246,11 +246,9 @@ uint16_t startmillisoffset = 0;
 long lastSync = millis();
 long timeSyncInterval = 10000;
 void syncTime() {
-  //get a random server from the pool
   WiFi.hostByName(ntpServerName, timeServerIP);
 
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-  // wait to see if a reply is available
+  sendNTPpacket(timeServerIP);
   delay(1000);
 
   int cb = udp.parsePacket();
@@ -261,22 +259,13 @@ void syncTime() {
   else {
     Serial.print("packet received, length=");
     Serial.println(cb);
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
+    udp.read(packetBuffer, NTP_PACKET_SIZE);
 
     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
     unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
     unsigned long secsSince1900 = highWord << 16 | lowWord;
-    // now convert NTP time into everyday time:
     Serial.print("Time: ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
     unsigned long epoch = secsSince1900 - 2208988800UL;
-    // print Unix time:
     Serial.println(epoch);
 
     TimeChangeRule myDST = {"CEST", Last, Sun, Mar, 2, 120};    //Daylight time = UTC + 2 hours
@@ -299,23 +288,17 @@ void syncTime() {
 unsigned long sendNTPpacket(IPAddress& address)
 {
   Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[0] = 0b11100011;
+  packetBuffer[1] = 0;
+  packetBuffer[2] = 6;
+  packetBuffer[3] = 0xEC;
   packetBuffer[12]  = 49;
   packetBuffer[13]  = 0x4E;
   packetBuffer[14]  = 49;
   packetBuffer[15]  = 52;
 
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.beginPacket(address, 123);
   udp.write(packetBuffer, NTP_PACKET_SIZE);
   udp.endPacket();
 }
@@ -559,4 +542,3 @@ void loop() {
     }
     server.handleClient();
 }
-
